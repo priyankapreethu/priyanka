c0d823e4467c0218f8f4de2839cd7d93e6c2462d
priyankapreethu <51438100+priyankapreethu@users.noreply.github.com>
2019-06-14 09:36:26 +0530

    Update sum_of_n_numbers.c

--- sum_of_n_numbers.c
+++ sum_of_n_numbers.c
@@ -3,8 +3,8 @@ void input(int *n,int *p)
 {
         printf("enter the size of array\n");
         scanf("%d",n);
-        printf("enter %d elements\n",n)
-        for(int i=0;i<n;i++)
+        printf("enter %d elements\n",n);
+        for(int i=0;i<*n;i++)
         {
                 scanf("%d",p);
                 p++;
@@ -14,9 +14,11 @@ int main()
 {
         int *p,n,a[10],res;
         p=a;
-        input(int &n,p);
+        input(&n,p);
         for(int i=0;i<n;i++)
                 printf("%d",p++);
         //sum(n,p,&res);
         //output(n,p,res);
 }
+~
+
