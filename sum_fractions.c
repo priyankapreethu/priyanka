791b88a00a7fc428aaec935534d201e291b1b5d0
priyanka <priyankapreethu2222@gmail.com>
2019-06-15 08:54:54 +0530

    commited file

--- sum_fractions.c
+++ sum_fractions.c
@@ -19,4 +19,26 @@ int main()
         struct frac f1,f2,res;
         void input(&f1,&f2);
         return 0;
+#include<stdio.h>
+/* Give descriptive names */
+struct frac
+{
+        int n;
+        int d;
+};
+void input(struct *f1,struct *f2)
+{
+        printf("enter the first fraction\n");
+        scanf("%d%d",f1->n,f2->d);
+        printf("enter the second fraction");
+        scanf("%d%d",f2->n,f2->d);
+        printf("%d/%d/n"f1->n,f1->d);
+        printf("%d/%d/n",f2->n,f2->d);
 }
+int main()
+{
+        struct frac f1,f2,res;
+        void input(&f1,&f2);
+        return 0;
+}
+
