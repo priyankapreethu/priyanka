48c35692e553b7c06843c5e1c5cb7dcbc78dfa47
priyankapreethu <51438100+priyankapreethu@users.noreply.github.com>
2019-06-18 08:31:18 +0530

    Update sum_fractions.c

--- sum_fractions.c
+++ sum_fractions.c
@@ -10,46 +10,43 @@ void input(struct frac *f1,struct frac *f2)
         scanf("%d%d",&f1->num,&f1->den);
         printf("enter the second fraction");
         scanf("%d%d",&f2->num,&f2->den);
-        printf("%d/%d\n",f1->num,f1->den);
-        printf("%d/%d\n",f2->num,f2->den);
 }
 void getgcd( int a,int b,int * gcd)
 {
-	int n1=a;
+        int n1=a;
         int n2=b;
-	while(n1!=n2)
-	{
-		if(n1>n2)
-			n1-=n2;
-		else
-			n2-=n1;
-	}
-	*gcd=n1;
+        while(n1!=n2)
+        {
+                if(n1>n2)
+                        n1-=n2;
+                else
+                        n2-=n1;
+        }
+        *gcd=n1;
 
 }
 void frac_sum(struct frac f1,struct frac f2,struct frac *res )
 {
-	int gcd,temp;
-	getgcd(f1.den,f2.den,&gcd);
-	printf("gcd is %d",gcd);
-	temp =f1.num*gcd;
-	temp/=f1.den;
-	res->num=f2.num*gcd;
-	res->num/=f2.den;
-	res->num+=temp;
-	res->den=(f1.den*f2.den)/gcd;
-	printf("%d/%d",res->num,res->den);
+        int gcd,temp;
+        getgcd(f1.den,f2.den,&gcd);
+        res->den=(f1.den*f2.den)/gcd;
+        temp =f1.num*res->den;
+        temp/=f1.den;
+        res->num=f2.num*res->den;
+        res->num/=f2.den;
+        res->num+=temp;
 
+}
+void output(struct frac n1,struct frac n2,struct frac res)
+{
+          printf("%d/%d+%d/%d=%d/%d",n1.num,n1.den,n2.num,n2.den,res.num,res.den);
 }
 int main()
 {
-	struct frac n1,n2,sum;
+        struct frac n1,n2,sum;
         input(&n1,&n2);
-        printf("%d/%d\n",n1.num,n1.den);
-        printf("%d/%d\n",n2.num,n2.den);
-	frac_sum(n1,n2,&sum);
-	return 0;
-	
+        frac_sum(n1,n2,&sum);
+        output(n1,n2,sum);
+        return 0;
+
 }
-
-
