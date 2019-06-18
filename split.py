33c65d2e9931d6b29c44ffbbbab22d5ca9999945
priyankapreethu <51438100+priyankapreethu@users.noreply.github.com>
2019-06-18 12:54:51 +0530

    Update split.py

--- split.py
+++ split.py
@@ -3,12 +3,27 @@ def getInput(lst):
     size=int(input("enter the size"));
     print(size)
     for i in range(int(size)):
-        val=(input("number:"))
+        val=(input("enter string:"))
         lst.append(val)
-        print(lst)
+    print(lst)
     return lst;
-
+def split(lst,s1,s2):
+    temp=[]
+    for i in lst:
+        temp=i.split('=');
+        s1.append(temp[0]);
+        s2.append(temp[1])
+    return s1,s2
+def output(s1,s2):
+    print("list 1:",s1)
+    print("list 2:",s2)
 def main():
+    res=0;
     lst=[]
+    s1=[];s2=[];
     lst=getInput(lst)
+    s1,s2=split(lst,s1,s2)
+    output(s1,s2);
 main()
+
+    
