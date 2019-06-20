7b25c80173738b4b593971f826cc7871e6bc88c5
priyanka <priyankapreethu2222@gmail.com>
2019-06-20 08:36:56 +0530

    Coded get input method

---
+++ sum_n_fractions.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+struct frac
+{
+	int num;
+	int den;
+}
+void getInput(struct frac *f,int *n)
+{
+	printf("enter the number of fractions\n");
+	scanf("%d",n);
+	for(int  i=0;i<*n;i++)
+	{
+		sacnf("%d %d",&f->num,&f->den);
+		f++;
+	}
+}
+void main()
+{
+	int n;
+	struct frac f[10];
+	void getInput(f1,n);
+	for(int  i=0;i<*n;i++)
+        {
+                printf("%d /%d\t",f->num,f->den);
+                f++;
+        }
+
+	
+}
