03eb13d864811064a9de2fc4a81c59a45f8f7add
priyanka <priyankapreethu2222@gmail.com>
2019-06-28 08:23:03 +0530

    output method

--- p1_area_rectangle.c
+++ p1_area_rectangle.c
@@ -8,7 +8,7 @@ struct rectangle
 {
 	struct vert v[3];
 };
-void getInput(int n,struct rectangle P[])
+void getInput(int n,struct rectangle P[n])
 {
 	int i,j;
 	for(i=0;i<n;i++)
@@ -20,9 +20,22 @@ void getInput(int n,struct rectangle P[])
 	}
 }
 
-void compute(int n,struct rectangle P[])
+/*void compute(int n,struct rectangle P[])
 {
 	
+}*/
+void output(int n,struct rectangle P[n])
+{
+	int area=0;
+	for(int i=0;i<n;i++)
+	{
+		printf("area of a rectangle with vertices ");
+		for(int j=0;j<3;j++)
+		{
+			printf("(%0.1f,%0.1f),",P[i].v[j].x,P[i].v[j].y);
+		}
+		printf(" is %d\n",area);
+	}
 }
 int main()
 {
@@ -31,5 +44,6 @@ int main()
 	scanf("%d",&n);
 	struct rectangle P[n];
 	getInput(n,P);
-	compute(n,P);
+	//compute(n,P);
+	output(n,P);
 }
