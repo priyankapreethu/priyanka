51d184d9049fd73d79884a62c4e53b451c6a17f0
priyanka <priyankapreethu2222@gmail.com>
2019-06-28 10:48:32 +0530

    data structure

---
+++ p2_egyptian_fraction.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void getInput(int n,int frac[n][n])
+{
+	printf("display method\n");
+}
+int main()
+{
+	int n;
+	printf("enter the number of instances\n");
+	scanf("%d",&n);
+	int frac[n][n];
+	getInput(n,frac);
+	return 0;
+}
