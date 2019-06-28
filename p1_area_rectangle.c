2731c606a7a1593457f72b7a5df0a15f12f1a802
priyanka <priyankapreethu2222@gmail.com>
2019-06-28 09:44:17 +0530

    code with output

--- p1_area_rectangle.c
+++ p1_area_rectangle.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<math.h>
 struct vert
 {
 	float x;
@@ -19,14 +20,44 @@ void getInput(int n,struct rectangle P[n])
 		}
 	}
 }
-
-/*void compute(int n,struct rectangle P[])
+float largest(float n1,float n2,float n3)
 {
-	
-}*/
-void output(int n,struct rectangle P[n])
+	if(n1>n2&&n1>n3)
+		return n1;
+	else if(n2>n3&&n2>n1)
+		return n2;
+	else 
+		return n3;
+}
+void compute(int n,struct rectangle P[],float area[n])
 {
-	int area=0;
+	float d1,d2,d3;
+	for(int i=0;i<n;i++)
+	{
+		d1=sqrt(((P[i].v[0].x-P[i].v[1].x)*(P[i].v[0].x-P[i].v[1].x))+((P[i].v[0].y-P[i].v[1].y)*(P[i].v[0].y-P[i].v[1].y)));
+                d2=sqrt(((P[i].v[1].x-P[i].v[2].x)*(P[i].v[1].x-P[i].v[2].x))+((P[i].v[1].y-P[i].v[2].y)*(P[i].v[1].y-P[i].v[2].y)));
+                d3=sqrt(((P[i].v[0].x-P[i].v[2].x)*(P[i].v[0].x-P[i].v[2].x))+((P[i].v[0].y-P[i].v[2].y)*(P[i].v[0].y-P[i].v[2].y)));
+		printf("d1===%f\n",d1);
+		printf("d2===%f\n",d2);
+		printf("d3===%f\n",d3);
+		float large =largest(d1,d2,d3);
+		printf("large==%f\n",large);
+		if(large==d1)
+		{		
+			area[i]=d2*d3;
+		}
+		else if(large==d2)
+		{
+			area[i]=d1*d3;
+		}
+		else
+		{
+			area[i]=d1*d2;
+		}
+	}
+}
+void output(int n,struct rectangle P[n],float area[n])
+{
 	for(int i=0;i<n;i++)
 	{
 		printf("area of a rectangle with vertices ");
@@ -34,7 +65,7 @@ void output(int n,struct rectangle P[n])
 		{
 			printf("(%0.1f,%0.1f),",P[i].v[j].x,P[i].v[j].y);
 		}
-		printf(" is %d\n",area);
+		printf(" is %f\n",area[i]);
 	}
 }
 int main()
@@ -43,7 +74,8 @@ int main()
 	printf("enter the number of rectangles\n");
 	scanf("%d",&n);
 	struct rectangle P[n];
+	float area[n];
 	getInput(n,P);
-	//compute(n,P);
-	output(n,P);
+	compute(n,P,area);
+	output(n,P,area);
 }
