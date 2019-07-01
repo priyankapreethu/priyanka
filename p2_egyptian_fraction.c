b9547529f2a06c84fd37cab1139f63d22d770810
priyanka <priyankapreethu2222@gmail.com>
2019-07-01 12:01:26 +0530

    final code

--- p2_egyptian_fraction.c
+++ p2_egyptian_fraction.c
@@ -22,6 +22,17 @@ void getInput(int n,struct egyptian_fract frac[n])
 		}
 	}
 }
+int getgcd(int n1,int n2)
+{
+        int a,b;
+        a=n1;
+        b=n2;
+        if (b != 0)
+               return getgcd(b, a%b);
+        else
+               return a;
+}
+
 void compute(int n,struct egyptian_fract frac[n],struct res_fraction r[n])
 {
 	for(int i=0;i<n;i++)
@@ -29,17 +40,17 @@ void compute(int n,struct egyptian_fract frac[n],struct res_fraction r[n])
 		int temp1=1;
 		int temp2=0;
 		int gcd;
-			for(int j=0;j<frac[i].m;j++)
-			{
-				temp1*=frac[i].d[j];
-			}
-		printf("%d\n",temp1);
 		for(int j=0;j<frac[i].m;j++)
-                	{
-                        	temp2+=temp1/(frac[i].d[j]);
-                	}	
-		printf("%d\n",temp2);
-		
+		{
+			temp1*=frac[i].d[j];
+		}
+		for(int j=0;j<frac[i].m;j++)
+                {
+                       	temp2+=temp1/(frac[i].d[j]);
+                }
+		gcd=getgcd(temp1,temp2);
+		r[i].num=temp2/gcd;
+		r[i].den=temp1/gcd;
 	}
 }
 void output(int n,struct egyptian_fract frac[n],struct res_fraction r[n])
@@ -50,7 +61,7 @@ void output(int n,struct egyptian_fract frac[n],struct res_fraction r[n])
                 {
 			printf("1/%d+",frac[i].d[j]);
                 }
-		printf("=\n");
+		printf("=%d/%d\n",r[i].num,r[i].den);
 	}
 }
 int main()
