2a62e06f5969cd14b5af408dc2751e2298abccac
priyanka <priyankapreethu2222@gmail.com>
2019-07-01 10:56:23 +0530

    getinput method for a structure array

--- p2_egyptian_fraction.c
+++ p2_egyptian_fraction.c
@@ -2,20 +2,18 @@
 struct egyptian_fract
 {
 	int m;
-	int f[10];
+	int d[10];
 };
 
 void getInput(int n,struct egyptian_fract frac[n])
 {
-	int i,j,m;
-	printf("display method\n");
-	for(i=0;i<n;i++)
+	for(int i=0;i<n;i++)
 	{
-		printf("enter how many unit fractions\n");
-		scanf("%d",&m);
-		for(int j=0;j<m;j++)
+		printf("enter how many unit fractions in %d instance\n",i+1);
+		scanf("%d",&frac[i].m);
+		for(int j=0;j<frac[i].m;j++)
 		{
-			scanf("%d",&f[j]);
+			scanf("%d",&frac[i].d[j]);
 		}
 	}
 }
@@ -24,7 +22,7 @@ int main()
 	int n;
 	printf("enter the number of instances\n");
 	scanf("%d",&n);
-	struct fraction frac[n];
+	struct egyptian_fract frac[n];
 	getInput(n,frac);
 	return 0;
 }
