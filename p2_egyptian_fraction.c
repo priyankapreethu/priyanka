a1e5fef2ffe662dc9fec4c1af1b44e25bd6e0c02
priyanka <priyankapreethu2222@gmail.com>
2019-07-01 11:53:01 +0530

    finding a denominator and numerator

--- p2_egyptian_fraction.c
+++ p2_egyptian_fraction.c
@@ -4,7 +4,12 @@ struct egyptian_fract
 	int m;
 	int d[10];
 };
+struct res_fraction
+{
+	int num;
+	int den;
 
+};
 void getInput(int n,struct egyptian_fract frac[n])
 {
 	for(int i=0;i<n;i++)
@@ -17,12 +22,46 @@ void getInput(int n,struct egyptian_fract frac[n])
 		}
 	}
 }
+void compute(int n,struct egyptian_fract frac[n],struct res_fraction r[n])
+{
+	for(int i=0;i<n;i++)
+	{
+		int temp1=1;
+		int temp2=0;
+		int gcd;
+			for(int j=0;j<frac[i].m;j++)
+			{
+				temp1*=frac[i].d[j];
+			}
+		printf("%d\n",temp1);
+		for(int j=0;j<frac[i].m;j++)
+                	{
+                        	temp2+=temp1/(frac[i].d[j]);
+                	}	
+		printf("%d\n",temp2);
+		
+	}
+}
+void output(int n,struct egyptian_fract frac[n],struct res_fraction r[n])
+{
+	for(int i=0;i<n;i++)
+        {
+                for(int j=0;j<frac[i].m;j++)
+                {
+			printf("1/%d+",frac[i].d[j]);
+                }
+		printf("=\n");
+	}
+}
 int main()
 {
 	int n;
 	printf("enter the number of instances\n");
 	scanf("%d",&n);
 	struct egyptian_fract frac[n];
+	struct res_fraction r[n];
 	getInput(n,frac);
+	compute(n,frac,r);
+	output(n,frac,r);
 	return 0;
 }
