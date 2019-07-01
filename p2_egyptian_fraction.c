2bbb950e951b0db1008bbfb4be4f3ac9974f3d4f
priyanka <priyankapreethu2222@gmail.com>
2019-07-01 10:49:16 +0530

    datastructure using a fraction

--- p2_egyptian_fraction.c
+++ p2_egyptian_fraction.c
@@ -1,14 +1,30 @@
 #include<stdio.h>
-void getInput(int n,int frac[n][n])
+struct egyptian_fract
 {
+	int m;
+	int f[10];
+};
+
+void getInput(int n,struct egyptian_fract frac[n])
+{
+	int i,j,m;
 	printf("display method\n");
+	for(i=0;i<n;i++)
+	{
+		printf("enter how many unit fractions\n");
+		scanf("%d",&m);
+		for(int j=0;j<m;j++)
+		{
+			scanf("%d",&f[j]);
+		}
+	}
 }
 int main()
 {
 	int n;
 	printf("enter the number of instances\n");
 	scanf("%d",&n);
-	int frac[n][n];
+	struct fraction frac[n];
 	getInput(n,frac);
 	return 0;
 }
